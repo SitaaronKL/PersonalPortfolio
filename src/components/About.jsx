8792dd285db683a13173f79debf55da08810c8ee
Dhruv <dhruvbhatia115@gmail.com>
2025-04-02 13:15:34 -0400

    fix: updated constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,45 +1,43 @@
 import React from 'react'
 import Tilt from 'react-parallax-tilt'
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services, stuff } from '../constants/constants';
-import { fadeIn,textVariant } from '../utils/motion';
+import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
-const ServiceCard = ({index, title, icon}) => {
-  return(
+const ServiceCard = ({ index, title, icon }) => {
+  return (
     <Tilt className='xs:w-[250px] w-full'>
       <motion.div
-        variants={fadeIn("right", "spring",0.75 * index, 0.5)}
-        className={`w-full ${
-          index===0 ? `vision-gradient`:
-          index===1 ? `blue-red-gradient`:
-          index===2 ? `peter-quill-gradient`:
-          `dr-strange-gradient`
-        } p-[1px] rounded-[20px] shadow-card`}
+        variants={fadeIn("right", "spring", 0.75 * index, 0.5)}
+        className={`w-full ${index === 0 ? `vision-gradient` :
+            index === 1 ? `blue-red-gradient` :
+              index === 2 ? `peter-quill-gradient` :
+                `dr-strange-gradient`
+          } p-[1px] rounded-[20px] shadow-card`}
       >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450
-        }}
-        className='bg-tertiary rounded-[20px]
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px]
         py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
-        <img
-        src={icon}
-        alt={title}
-        className={`object-contain ${
-        index === 0 ? 'w-30 h-30' :
-        index === 1 ? 'w-30 h-30' :
-        index === 2 ? 'w-30 h-30' :
-        index === 3 ? 'w-30 h-30': 'w-16 h-16'
-        }`}
-/>
-        <h3 className='text0white text-[20px] font-bold text-center'>{title}</h3>
-      </div>
+          <img
+            src={icon}
+            alt={title}
+            className={`object-contain ${index === 0 ? 'w-30 h-30' :
+                index === 1 ? 'w-30 h-30' :
+                  index === 2 ? 'w-30 h-30' :
+                    index === 3 ? 'w-30 h-30' : 'w-16 h-16'
+              }`}
+          />
+          <h3 className='text0white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
 
 
       </motion.div>
@@ -55,19 +53,19 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Who are you??</h2>
       </motion.div>
       <motion.p
-        variants={fadeIn("","",0.1,1)}
-        className='mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]' 
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]'
       >
         {stuff}
       </motion.p>
       <div className='mt-20 flex justify-center flex-wrap gap-10'>
-        {services.map((service,index) => (
-          <ServiceCard key={service.title} index = 
-          {index} {...service} />
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index=
+            {index} {...service} />
         ))}
       </div>
     </>
   )
 }
 
-export default SectionWrapper(About,"about")
+export default SectionWrapper(About, "about")
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,306 +1,306 @@
 import {
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    threejs,
-    spiderman,
-    guardian,
-    media,
-    vision,
-    applifyai_logo,
-    ameritax,
-    rutgers,
-    medify,
-    psr,
-    cjcphacks,
-    naren,
-    max,
-    vodify,
-    cvrve,
-    pantheon
-  } from "../assets";
-  
-  export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-  
-  const services = [
-    {
-      title: "ML Engineer",
-      icon: vision,
-    },
-    {
-      title: "Fullstack Web Developer",
-      icon: spiderman,
-    },
-    {
-      title: "Flutter/ReactN Developer",
-      icon: guardian,
-    },
-    {
-      title: "Content Creator",
-      icon: media,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-  
-  ];
-  
-const stuff = "I’m currently a Presidential Scholar at the Honors College of Rutgers, double majoring in CS and Neuroscience since I love the idea of neuro-technology and NeuroAI. In my spare time, I make gaming content as a professional Valorant player (Sitaaron#raja), run my own fashion brand, and add to my random list of skills!"
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  threejs,
+  spiderman,
+  guardian,
+  media,
+  vision,
+  applifyai_logo,
+  ameritax,
+  rutgers,
+  medify,
+  psr,
+  cjcphacks,
+  naren,
+  max,
+  vodify,
+  cvrve,
+  pantheon
+} from "../assets";
 
-  const experiences = [
-    {
-      title: "Software Engineer",
-      company_name: "Rutgers University Computational Brain Lab",
-      icon: cvrve,
-      iconBg: "#383E56",
-      date: "March 2025 - Present",
-      points: [
-        "Working on the biggest student-to-tech aggregation platform.",
-        "Specifically created the hackathon dashboard with Next.js, global CSS styling, and build checks, to retain visual consistency and performance.",
-        "Implemented cross-domain authentication using Firebase (Google OAuth) and Supabase (PostgreSQL), reducing login in latency by 20%.",
-        "Configured Prisma with Supabase to enable real-time data logging infrastructure for over 75,000 daily visits."
-      ],
-    },
-    {
-      title: "Incoming Cohort",
-      company_name: "Pantheon.nyc",
-      icon: pantheon,
-      iconBg: "#383E56",
-      date: "April 2025 - Present",
-      points: [
-        "Accepted into the first cohort of Pantheon.nyc, a startup accelerator for the top students in the east coast to build their own startups/products."
-      ],
-    },
-    {
-      title: "Incoming Aresty Research Assistant",
-      company_name: "Rutgers University Computational Brain Lab",
-      icon: rutgers,
-      iconBg: "#383E56",
-      date: "June 2025 - Present",
-      points: [
-        "Selected to be part of Rutger's presitgious Aresty Research Assistant Program, where I will be working on 3D insectoid robotics and publishing/presenting the results."
-      ],
-    },
-    {
-      title: "Machine Learning Intern",
-      company_name: "Rutgers University Computational Brain Lab",
-      icon: rutgers,
-      iconBg: "#383E56",
-      date: "September 2024 - Present",
-      points: [
-        "Currently working on developing ML models to improve the diagnosis of neurodegenerative diseases for Robert Wood Johnson Hospital using PyTorch and TensorFlow.",
-        "Also responsible for preprocessing and analyzing large brain MRI datasets using NumPy and R to streamline our data pipeline and enhance efficiency by 25%."
-      ],
-    },
-    {
-      title: "Software Developer Intern ",
-      company_name: "Rutgers University Center for Turfgrass Science",
-      icon: rutgers,
-      iconBg: "#383E56",
-      date: " July 2024 – September 2024",
-      points: [
-        "Designed and maintained scalable data pipelines for root health dataset analysis, using SQL and Python to support ongoing research efforts on how we can make better soil!",
-        "Automated statistical analysis and created data visualizations with Jupyter Notebooks and Matplotlib, making experimental results easier to interpret for stakeholders."],
-    },
-    {
-      title: "SWE + ML + UI/UX Intern",
-      company_name: "Applify AI",
-      icon: applifyai_logo,
-      iconBg: "#E6DEDD",
-      date: "November 2023 – July 2024",
-      points: [
-        "I joined the start up as a software engineering intern but ended up working on a variety of projects/features.",
-        "Built the document upload system with FastAPI and AWS S3 to let users upload their writing samples, and fine-tuned machine learning models with PyTorch which ended up saving us $500/month.",
-        "Designed the UI/UX for the onboarding experience using Figma and Tailwind CSS, which increased our student user engagement by 15%!"
-      ],
-    },
-    {
-      title: "Software Engineer",
-      company_name: "AmeriTax Inc",
-      icon: ameritax,
-      iconBg: "#383E56",
-      date: "November 2021 – September 2023",
-      points: [
-       "Made the company's website from scratch using React and Tailwind CSS for a responsive and user-friendly experience!",
-       "Implemented the backend with so that clients could log in and see their data such as their tax returns and processed financial statements, and even check the progress of their payrolls!",
-       "Deployed the website on AWS cloud services to make sure it was always up and running for the 4000+ clients we had!",
-       "Made numerous other software projects for the company using OpenCV to automate data entry tasks, decreasing overall payroll processing time by 30%!"
-      ],
-    }
-  ];
-  
-  const testimonials = [
-    {
-      testimonial:
-        "Most flexible kid I've ever met. He was part of our social media, design, and AI team doing random but damn good work. Any other startup or company would be lucky to have Dhruv in their ranks. ",
-      name: "Max Tang",
-      designation: "CEO",
-      company: "ApplifyAI",
-      image: max,
-    },
-    {
-      testimonial:
-        "He joined us at the lab before he was even a student, helping out with numerous tasks and showed real initiative and work ethic.",
-      name: "Dr.Stephanie Rossi",
-      designation: "Research Assiociate",
-      company: "Rutgers Center for Turfgrass",
-      image: "https://plantbiology.rutgers.edu/sites/default/files/styles/3x4_one_third_full_1024_2x/public/2024-08/SRossi-portrait.jpg?h=18e44ed0&itok=f8cVZqwF",
-    },
-    {
-      testimonial:
-        "Dhruv was incredibly reliable, learning everything he needed on the fly to build AmeriTax's website, and delivered with everything I needed.",
-      name: "Narendra Lalwani",
-      designation: "CEO",
-      company: "AmeriTax",
-      image: naren,
-    },
-  ];
-  
-  const projects = [
-    {
-      name: "Co-Founded Vodify.gg",
-      description:
-        "A startup dedicated to providing easy access to gaming coaching services for everyone. Accepted into Foundess's Hatch Accelerator W25 Cohort and backend ML models were accquired for $120,000. (No longer active)",
-      tags: [
-        {
-          name: "fullstack",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "nextjs",
-          color: "green-text-gradient",
-        },
-      ],
-      image: vodify,
-      source_code_link: "https://www.vodify.gg/",
-    },
-    {
-      name: "HackNJIT 2023 - Medify",
-      description:
-        "An AI powered hardware hack that got our team 3rd place as freshman at HackNJIT 2023! It allows a user to scan a wound and be given a accurate diagnosis and narrated treatment plan based on the resourcs available to them via a Rasperry Pi contraption and our web app!",
-      tags: [
-        {
-          name: "fullstack",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "raspberry-pi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "pytorch",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: medify,
-      source_code_link: "https://devpost.com/software/medify-khoaid",
-    },
-    {
-      name: "Team Captain - Princeton Soccer Robotics",
-      description:
-        "Led team Dragons in PSR during 2023, where I led a team of 4 to create and design a autonomous soccer robot that placed 5th in the RoboCupJr USA nationals!", 
-      tags: [
-        {
-          name: "robotics",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "arduino/c++",
-          color: "green-text-gradient",
-        },
-        {
-          name: "pcb design",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: psr,
-      source_code_link: "https://soccer-robotics.github.io/psr/",
-    },
-    {
-      name: "Founder - CJCP Hacks",
-      description:
-        "Led my high school's CS club, taught hundreds of lower classmen how to code and hosted annual hackathons with 100+ participants across the tristate area, $9k in prizes, and 10+ workshops!",
-      tags: [
-        {
-          name: "leadership",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "python/java",
-          color: "green-text-gradient",
-        },
-        {
-          name: "fundraising",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: cjcphacks,
-      source_code_link: "https://www.cjcphacks.org/",
-    },
-  ];
-  
-  export { services, technologies, experiences, testimonials, projects, stuff};
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "ML Engineer",
+    icon: vision,
+  },
+  {
+    title: "Fullstack Web Developer",
+    icon: spiderman,
+  },
+  {
+    title: "Flutter/ReactN Developer",
+    icon: guardian,
+  },
+  {
+    title: "Content Creator",
+    icon: media,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+];
+
+const stuff = "I’m currently a Presidential Scholar at the Honors College of Rutgers, double majoring in CS and Neuroscience since I love the idea of neuro-technology and NeuroAI. In my spare time, I make content on Youtube/Tiktok/Twitch, run my own fashion brand, and add to my random list of skills!"
+
+const experiences = [
+  {
+    title: "Software Engineer Intern",
+    company_name: "Rutgers University Computational Brain Lab",
+    icon: cvrve,
+    iconBg: "#383E56",
+    date: "March 2025 - Present",
+    points: [
+      "Working on the biggest student-to-tech aggregation platform.",
+      "Specifically created the hackathon dashboard with Next.js, global CSS styling, and build checks, to retain visual consistency and performance.",
+      "Implemented cross-domain authentication using Firebase (Google OAuth) and Supabase (PostgreSQL), reducing login in latency by 20%.",
+      "Configured Prisma with Supabase to enable real-time data logging infrastructure for over 75,000 daily visits."
+    ],
+  },
+  {
+    title: "Incoming Cohort",
+    company_name: "Pantheon.nyc",
+    icon: pantheon,
+    iconBg: "#383E56",
+    date: "April 2025 - Present",
+    points: [
+      "Accepted into the first cohort of Pantheon.nyc, a startup accelerator for the top students in the east coast to build their own startups/products."
+    ],
+  },
+  {
+    title: "Incoming Aresty Research Assistant",
+    company_name: "Rutgers University Computational Brain Lab",
+    icon: rutgers,
+    iconBg: "#383E56",
+    date: "June 2025 - Present",
+    points: [
+      "Selected to be part of Rutger's presitgious Aresty Research Assistant Program, where I will be working on 3D insectoid robotics and publishing/presenting the results."
+    ],
+  },
+  {
+    title: "Machine Learning Intern",
+    company_name: "Rutgers University Computational Brain Lab",
+    icon: rutgers,
+    iconBg: "#383E56",
+    date: "September 2024 - Present",
+    points: [
+      "Currently working on developing ML models to improve the diagnosis of neurodegenerative diseases for Robert Wood Johnson Hospital using PyTorch and TensorFlow.",
+      "Also responsible for preprocessing and analyzing large brain MRI datasets using NumPy and R to streamline our data pipeline and enhance efficiency by 25%."
+    ],
+  },
+  {
+    title: "Software Developer Intern ",
+    company_name: "Rutgers University Center for Turfgrass Science",
+    icon: rutgers,
+    iconBg: "#383E56",
+    date: " July 2024 – September 2024",
+    points: [
+      "Designed and maintained scalable data pipelines for root health dataset analysis, using SQL and Python to support ongoing research efforts on how we can make better soil!",
+      "Automated statistical analysis and created data visualizations with Jupyter Notebooks and Matplotlib, making experimental results easier to interpret for stakeholders."],
+  },
+  {
+    title: "SWE + ML + UI/UX Intern",
+    company_name: "Applify AI",
+    icon: applifyai_logo,
+    iconBg: "#E6DEDD",
+    date: "November 2023 – July 2024",
+    points: [
+      "I joined the start up as a software engineering intern but ended up working on a variety of projects/features.",
+      "Built the document upload system with FastAPI and AWS S3 to let users upload their writing samples, and fine-tuned machine learning models with PyTorch which ended up saving us $500/month.",
+      "Designed the UI/UX for the onboarding experience using Figma and Tailwind CSS, which increased our student user engagement by 15%!"
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "AmeriTax Inc",
+    icon: ameritax,
+    iconBg: "#383E56",
+    date: "November 2021 – September 2023",
+    points: [
+      "Made the company's website from scratch using React and Tailwind CSS for a responsive and user-friendly experience!",
+      "Implemented the backend with so that clients could log in and see their data such as their tax returns and processed financial statements, and even check the progress of their payrolls!",
+      "Deployed the website on AWS cloud services to make sure it was always up and running for the 4000+ clients we had!",
+      "Made numerous other software projects for the company using OpenCV to automate data entry tasks, decreasing overall payroll processing time by 30%!"
+    ],
+  }
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Most flexible kid I've ever met. He was part of our social media, design, and AI team doing random but damn good work. Any other startup or company would be lucky to have Dhruv in their ranks. ",
+    name: "Max Tang",
+    designation: "CEO",
+    company: "ApplifyAI",
+    image: max,
+  },
+  {
+    testimonial:
+      "He joined us at the lab before he was even a student, helping out with numerous tasks and showed real initiative and work ethic.",
+    name: "Dr.Stephanie Rossi",
+    designation: "Research Assiociate",
+    company: "Rutgers Center for Turfgrass",
+    image: "https://plantbiology.rutgers.edu/sites/default/files/styles/3x4_one_third_full_1024_2x/public/2024-08/SRossi-portrait.jpg?h=18e44ed0&itok=f8cVZqwF",
+  },
+  {
+    testimonial:
+      "Dhruv was incredibly reliable, learning everything he needed on the fly to build AmeriTax's website, and delivered with everything I needed.",
+    name: "Narendra Lalwani",
+    designation: "CEO",
+    company: "AmeriTax",
+    image: naren,
+  },
+];
+
+const projects = [
+  {
+    name: "Co-Founded Vodify.gg",
+    description:
+      "A startup dedicated to providing easy access to gaming coaching services for everyone. Accepted into Foundess's Hatch Accelerator W25 Cohort and backend ML models were accquired for $120,000. (Sold/No longer active)",
+    tags: [
+      {
+        name: "fullstack",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nextjs",
+        color: "green-text-gradient",
+      },
+    ],
+    image: vodify,
+    source_code_link: "https://www.vodify.gg/",
+  },
+  {
+    name: "HackNJIT 2023 - Medify",
+    description:
+      "An AI powered hardware hack that got our team 3rd place as freshman at HackNJIT 2023! It allows a user to scan a wound and be given a accurate diagnosis and narrated treatment plan based on the resourcs available to them via a Rasperry Pi contraption and our web app!",
+    tags: [
+      {
+        name: "fullstack",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "raspberry-pi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "pytorch",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: medify,
+    source_code_link: "https://devpost.com/software/medify-khoaid",
+  },
+  {
+    name: "Team Captain - Princeton Soccer Robotics",
+    description:
+      "Led team Dragons in PSR during 2023, where I led a team of 4 to create and design a autonomous soccer robot that placed 5th in the RoboCupJr USA nationals!",
+    tags: [
+      {
+        name: "robotics",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "arduino/c++",
+        color: "green-text-gradient",
+      },
+      {
+        name: "pcb design",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: psr,
+    source_code_link: "https://soccer-robotics.github.io/psr/",
+  },
+  {
+    name: "Founder - CJCP Hacks",
+    description:
+      "Led my high school's CS club, taught hundreds of lower classmen how to code and hosted annual hackathons with 100+ participants across the tristate area, $9k in prizes, and 10+ workshops!",
+    tags: [
+      {
+        name: "leadership",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "python/java",
+        color: "green-text-gradient",
+      },
+      {
+        name: "fundraising",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: cjcphacks,
+    source_code_link: "https://www.cjcphacks.org/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects, stuff };
